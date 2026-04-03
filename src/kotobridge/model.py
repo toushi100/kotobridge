d5cf1336150430ad35890920f54a2e6f2a3c4941
Ahmed Salah <toushi2009@gmail.com>
2026-04-03 09:33:45 +0200

    Add Model class for Whisper integration with device detection

---
+++ src/kotobridge/model.py
@@ -0,0 +1,24 @@
+import torch
+import whisper
+
+class Model:
+    def __init__(self, model_name: str="large-v3"):
+        self.model_name = model_name
+        self.model = None
+        
+
+
+    def load_model(self):
+        self.device = self.detect_device()
+        print(f"Using device: {self.device}")
+        self.model = whisper.load_model(self.model_name, device=self.device)
+        print(f"Model Name: {self.model_name}")
+        return self.model
+
+    def detect_device(self):
+        if torch.backends.mps.is_available():
+            return "mps"
+        elif torch.cuda.is_available():
+            return "cuda"
+        else:
+            return "cpu"
