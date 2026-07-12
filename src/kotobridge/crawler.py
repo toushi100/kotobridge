b745b120ea3a5c1d6bcffc39213b7cab3799335e
Ahmed Salah (toushi) <toushi2009@gmail.com>
2026-07-12 21:46:28 +0300

    Add folder crawler to detect and log video and audio files

---
+++ src/kotobridge/crawler.py
@@ -0,0 +1,16 @@
+#this class is responsible for crawling the folder and getting all the video and audio files in it and in subfolders and logs them to the console
+import os
+class Crawler:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.files = []
+
+    def crawl(self):
+        for root, dirs, files in os.walk(self.folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if file_path.endswith(('.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.mp3', '.wav', '.aac')):
+                    self.files.append(file_path)
+                    print(f"Found file: {file_path}")
+    def get_count(self):
+        return len(self.files)
