1a92a9d5018d9ee66b74ff7487003c399b15c8b3
Ahmed Salah (toushi) <toushi2009@gmail.com>
2026-07-15 17:27:57 +0300

    Refactor translate and crawl commands to improve file path handling and database integration

--- src/kotobridge/cli.py
+++ src/kotobridge/cli.py
@@ -1,31 +1,66 @@
+import os
+
 import typer
 from kotobridge.translate import Translate
 import time
 from kotobridge.crawler import Crawler
+from db.init_db import DB
+
+STATUS_MAP = {
+    "discovered": 0,
+    "in_progress": 1,
+    "transcribed": 2,
+    "translated": 3
+}
 
 app = typer.Typer()
 
-@app.command()
-def translate(name: str ):
-    start_time = time.time()    
-    if name is None:
-        typer.echo("Please enter a valid text to translate")
-        return None
-    translate = Translate(name)
-    result = translate.translate()
 
-    end_time = time.time()
-    typer.echo(f"Time taken: {end_time - start_time} seconds")
-    return None
+
+
+@app.command()
+def translate(path: str ):
+    if is_folder(path):
+        crawl(path)
+    else:
+        start_time = time.time()    
+        if path is None:
+            typer.echo("Please enter a valid file path to translate")
+            return None
+        translate = Translate(path)
+        result = translate.translate()
+
+        end_time = time.time()
+        typer.echo(f"Time taken: {end_time - start_time} seconds")
+        return None
+
+
 @app.command()
 def crawl(folder_path: str):
+    db = DB()
+    db.connect()
+    
     if folder_path is None:
         typer.echo("Please enter a valid folder path")
         return None
     crawler = Crawler(folder_path)
     crawler.crawl()
     print(f"Total files found: {crawler.get_count()}")
+    crawler.store_discovered_files(db)
+    videos_count = db.query("SELECT * FROM videos")
+    print(f"Total files stored in database: {len(videos_count)}") 
     return None
+
+@app.command()
+def clear_db():
+    db = DB()
+    db.clear_db()
+    print("Database cleared")
+    return None
+
+def is_folder(path: str):
+    return os.path.isdir(path)
+
 @app.command()
 def main():
     # should get back to this later so the cli would be interactive
